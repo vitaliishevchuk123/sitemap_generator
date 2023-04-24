3923ea36c1947a899679f1cde488eed8bf886d64
vitaliishevchuk <vitaliishevchuk123@gmail.com>
2023-04-24 21:48:27 +0300

    start

---
+++ README.md
@@ -0,0 +1,17 @@
+
+## Dev tips
+```
+docker-compose build --no-cache
+docker-compose up --build --force-recreate --no-deps
+
+docker-compose down
+
+docker-compose up -d
+
+docker exec -it sitemap_generator_app bash
+
+//не заходячи в bash, просто в папці проекту перший раз дати пермішини
+sudo chmod -R 777  ./
+
+systemctl restart docker
+```
---
+++ app/Console/Commands/GenerateSitemapCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemapCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate-sitemap {url : The URL to generate sitemap for}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generating sitemaps for sites';
+
+    public function handle()
+    {
+        $validated = $this->validate();
+        SitemapGenerator::create($validated['url'])
+            ->getSitemap()->writeToDisk('public', md5($validated['url']) . '/sitemap.xml');
+        $this->info('The URL is: ' . $validated['url']);
+        $this->info('The Folder is: ' . url(md5($validated['url'])) . '/sitemap.xml');
+    }
+
+    protected function validate(): array
+    {
+        $validator = Validator::make([
+            'url' => $this->argument('url'),
+        ], [
+            'url' => ['required', 'url'],
+        ]);
+        if ($validator->fails()) {
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+            return [];
+        }
+        return $validator->validated();
+    }
+}
+
